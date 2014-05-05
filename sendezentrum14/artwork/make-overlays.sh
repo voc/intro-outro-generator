044b20ef85d0538e410ce9af0ec8cc51e856f39f
MaZderMind <github@mazdermind.de>
2014-05-05 21:12:05 +0200

    use signet for overlay

---
+++ sendezentrum14/artwork/make-overlays.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+inkscape --export-png=overlay-1024x576.png --export-width=1024 --export-height=576 overlay.svg
+inkscape --export-png=overlay-720x576.png  --export-width=720  --export-height=576 overlay.svg
