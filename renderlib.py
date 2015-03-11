be2cc2d56b9c430ac303f7c82fab7da50f0e9cf1
MaZderMind <github@mazdermind.de>
2015-03-11 19:24:45 +0100

    Trim Whitespace around the Title

--- renderlib.py
+++ renderlib.py
@@ -196,8 +196,8 @@ def events(scheduleUrl, titlemap={}):
 				# yield a tupel with the event-id, event-title and person-names
 				yield {
 					'id': int(event.get('id')),
-					'title': titlemap[id] if id in titlemap else event.find('title').text,
-					'subtitle': event.find('subtitle').text if event.find('subtitle') is not None else '',
+					'title': titlemap[id] if id in titlemap else event.find('title').text.strip(),
+					'subtitle': event.find('subtitle').text.strip() if event.find('subtitle') is not None else '',
 					'persons': personnames,
 					'personnames': ', '.join(personnames)
 				}
