a2f1ae8d3c7878a2c7cf7eb3f8484764345aaecf
MaZderMind <github@mazdermind.de>
2015-08-14 02:19:03 +0200

    fix ffmpeg. one time more

--- renderlib.py
+++ renderlib.py
@@ -129,7 +129,7 @@ def rendertask(task):
 	# invoke avconv aka ffmpeg and renerate a lossles-dv from the frames
 	#  if we're not in debug-mode, suppress all output
 	if task.outfile.endswith('.ts'):
-		cmd = 'cd {0} && ffmpeg -f image2 -i .frames/%04d.png -c:v mpeg2video -qscale 0 -aspect 16:9 -ar 48000 -ac 1 -f s16le -i /dev/zero -ar 48000 -ac 1 -f s16le -i /dev/zero -map 0:0 -map 1:0 -map 2:0 -shortest -f mpegts "{1}"'.format(task.workdir, task.outfile)
+		cmd = 'cd {0} && ffmpeg -f image2 -i .frames/%04d.png -ar 48000 -ac 1 -f s16le -i /dev/zero -ar 48000 -ac 1 -f s16le -i /dev/zero -map 0:0 -c:v mpeg2video -q:v 0 -aspect 16:9 -map 1:0 -map 2:0 -shortest -f mpegts "{1}"'.format(task.workdir, task.outfile)
 	else:
 		cmd = 'cd {0} && ffmpeg -ar 48000 -ac 2 -f s16le -i /dev/zero -f image2 -i .frames/%04d.png -target pal-dv -aspect 16:9 -shortest "{1}"'.format(task.workdir, task.outfile)
 
