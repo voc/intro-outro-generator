bae6bf5df41ed604c316c11b09c22153921ba60a
Daniel Molkentin <danimo@owncloud.com>
2016-04-04 21:33:12 +0200

    add preminary minilac design

---
+++ minilac16/__init__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+from renderlib import *
+from easing import *
+
+def introFrames(p):
+	move=50
+
+	# 4 Sekunden stehen lassen
+	frames = 2*fps
+	for i in range(0, frames):
+                yield (
+			('text', 'style',    'opacity', "%.4f" % 100),
+			('text', 'attr',     'transform', 'translate(%.4f, 0)' % 0),
+		)
+
+	# 3 Sekunde Text Fadeout
+	frames = 3*fps
+	for i in range(0, frames):
+		yield (
+			('text', 'style',    'opacity', "%.4f" % easeLinear(i, 1, -1, frames)),
+			('text', 'attr',     'transform', 'translate(%.4f, 0)' % easeInQuad(i, 0, move, frames)),
+		)
+
+	# two final frames
+	for i in range(0, 2):
+		yield (
+			('text', 'style',    'opacity', "%.4f" % 0),
+			('text', 'attr',     'transform', 'translate(%.4f, 0)' % move),
+		)
+
+def outroFrames(p):
+	# 5 Sekunden stehen bleiben
+	frames = 5*fps
+	for i in range(0, frames):
+		yield []
+
+def debug():
+	render(
+		'intro.svg',
+		'../intro.ts',
+		introFrames,
+		{
+			'$id': 2,
+			'$title': 'Essential Aspects on Mixing',
+			'$person': 'Jimson Drift'
+		}
+	)
+
+#	render(
+#		'outro.svg',
+#		'../outro.ts',
+#		outroFrames
+#	)
+
+def tasks(queue, args):
+	raise NotImplementedError('call with --debug to render your intro/outro')
