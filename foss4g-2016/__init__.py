e93d830404162d002634f0fa5d15fb1c9d6ca1d7
MaZderMind <git@mazdermind.de>
2016-04-14 01:58:30 +0200

    foss4g first draft

---
+++ foss4g-2016/__init__.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+from renderlib import *
+from easing import *
+
+def introFrames(p):
+	move=50
+
+	# five initial frames
+	for i in range(0, 5):
+		yield (
+			('text', 'style',    'opacity', "%.4f" % 0),
+			('text', 'attr',     'transform', 'translate(%.4f, 0)' % -move),
+		)
+
+	# 3 Sekunde Text Fadein
+	frames = 3*fps
+	for i in range(0, frames):
+		yield (
+			('text', 'style',    'opacity', "%.4f" % easeLinear(i, 0, 1, frames)),
+			('text', 'attr',     'transform', 'translate(%.4f, 0)' % easeOutQuad(i, move, -move, frames)),
+		)
+
+	# 2 Sekunden stehen lassen
+	frames = 2*fps
+	for i in range(0, frames):
+		yield ()
+
+	# 3 Sekunde Text Fadeout
+	frames = 3*fps
+	for i in range(0, frames):
+		yield (
+			('text', 'style',    'opacity', "%.4f" % easeLinear(i, 1, -1, frames)),
+			('text', 'attr',     'transform', 'translate(%.4f, 0)' % easeInQuad(i, 0, -move, frames)),
+		)
+
+	# two final frames
+	for i in range(0, 2):
+		yield (
+			('text', 'style',    'opacity', "%.4f" % 0),
+			('text', 'attr',     'transform', 'translate(%.4f, 0)' % move),
+		)
+
+def outroFrames(p):
+	# 3 Sekunden animation bleiben
+	frames = 5*fps
+
+	# five initial frames
+	for i in range(0, 5):
+		yield (
+			('g1', 'style',    'opacity', "%.4f" % 0),
+			('g2', 'style',    'opacity', "%.4f" % 0),
+			('g3', 'style',    'opacity', "%.4f" % 0),
+		)
+
+	# 3 Sekunden
+	frames = 3*fps
+	for i in range(0, frames):
+		yield (
+			('g1', 'style',    'opacity', "%.4f" % easeDelay(easeLinear, 0.0*fps, i, 0, 1, 2*fps)),
+			('g2', 'style',    'opacity', "%.4f" % easeDelay(easeLinear, 0.5*fps, i, 0, 1, 2*fps)),
+			('g3', 'style',    'opacity', "%.4f" % easeDelay(easeLinear, 1.0*fps, i, 0, 1, 2*fps)),
+		)
+
+	# five final frames
+	for i in range(0, 5):
+		yield (
+			('g1', 'style',    'opacity', "%.4f" % 1),
+			('g2', 'style',    'opacity', "%.4f" % 1),
+			('g3', 'style',    'opacity', "%.4f" % 1),
+		)
+
+def debug():
+	# render(
+	# 	'intro.svg',
+	# 	'../intro.ts',
+	# 	introFrames,
+	# 	{
+	# 		'$id': 65,
+	# 		'$title': 'Passwort, Karte oder Gesicht',
+	# 		'$subtitle': 'zur Sicherheit von Authentifizierungssystemen',
+	# 		'$personnames': 'starbug'
+	# 	}
+	# )
+
+	render(
+		'outro.svg',
+		'../outro.ts',
+		outroFrames
+	)
+
+def tasks(queue, args):
+	raise NotImplementedError('call with --debug to render your intro/outro')
