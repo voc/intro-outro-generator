372df0ec10ac7a65c7aa2d278290d9046c40f452
Peter Körner <pkoerner@seibert-media.net>
2017-08-05 14:34:27 +0200

    fix formatting of log-message

--- make-apple-motion.py
+++ make-apple-motion.py
@@ -183,7 +183,7 @@ def finalize_job(job_id, event):
 
 active_jobs = []
 
-print("enqueuing {} jobs into compressor", len(events))
+print("enqueuing {} jobs into compressor".format(len(events)))
 for event in events:
 	if args.ids and event['id'] not in args.ids:
 		continue
