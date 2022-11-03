be7ac0dfdeba0e4f43e0ea1e507f841ea7defc79
Peter Körner <pkoerner@seibert-media.net>
2022-11-03 23:55:20 +0100

    reduce sleep interval

--- make-apple-motion.py
+++ make-apple-motion.py
@@ -235,7 +235,7 @@ for event in filtered_events:
 print("waiting for rendering to complete")
 
 while len(active_jobs) > 0:
-    time.sleep(15)
+    time.sleep(10)
     active_jobs, finished_jobs = filter_finished_jobs(active_jobs)
 
     print("{} jobs in queue, {} ready to finalize".format(len(active_jobs), len(finished_jobs)))
